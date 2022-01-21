49c7fa388c5639c6de903285669a6876b2463411
inni <97818356+inni-iii@users.noreply.github.com>
2022-01-21 14:39:24 +0900

    Update 4-2.py

--- 이것이 취업을 위한 코딩테스트다_with python/4. 구현/4-2.py
+++ 이것이 취업을 위한 코딩테스트다_with python/4. 구현/4-2.py
@@ -1,1 +1,13 @@
 # my_code
+
+N = int(input())
+
+count = 0
+
+for i in range(N+1):
+  for j in range(60):
+    for k in range(60):
+      if '3' in str(k) + str(j) + str(i):
+        count += 1
+
+print(count)
