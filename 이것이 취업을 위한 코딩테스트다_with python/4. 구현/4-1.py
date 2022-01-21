2e5e69f636490314b81ada9514a00ee9c38619f0
inni <97818356+inni-iii@users.noreply.github.com>
2022-01-21 13:57:30 +0900

    Update 4-1.py

--- 이것이 취업을 위한 코딩테스트다_with python/4. 구현/4-1.py
+++ 이것이 취업을 위한 코딩테스트다_with python/4. 구현/4-1.py
@@ -1,1 +1,30 @@
 # my_code
+
+N = int(input())
+A_list = list(input().split())
+
+start = [1, 1]
+
+for i in A_list:
+  if i == 'L':
+    if start[1] == 1:
+      continue
+    else:
+      start[1] -= 1
+  if i == 'R':
+    if start[1] == 5:
+      continue
+    else:
+      start[1] += 1
+  if i == 'U':
+    if start[0] == 1:
+      continue
+    else:
+      start[0] -= 1
+  if i == 'D':
+    if start[0] == 5:
+      continue
+    else:
+      start[0] += 1
+
+print(start[0], start[1])
