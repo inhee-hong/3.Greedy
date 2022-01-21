02bfd4985a61f31278cd73f34e21622eb504b224
inni <97818356+inni-iii@users.noreply.github.com>
2022-01-21 15:26:59 +0900

    3. 왕실의 나이트

--- 이것이 취업을 위한 코딩테스트다_with python/4. 구현/4-3.py
+++ 이것이 취업을 위한 코딩테스트다_with python/4. 구현/4-3.py
@@ -1,1 +1,22 @@
 # my_code
+
+A = input()
+
+count = 0
+A_new = []
+
+dict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8 }
+
+A_new.append(dict[A[0]])
+A_new.append(int(A[1]))
+
+move = [[-2, 1], [-2, -1], [2, 1], [2, -1], [-1, -2],[-1, 2], [1, -2],[1, 2]]
+
+for i in range(8):
+  A_new2 = []
+  A_new2.append(A_new[0] + move[i][0])
+  A_new2.append(A_new[1] + move[i][1])
+  if A_new2[0] > 0 and A_new2[1] > 0 and A_new2[0] < 9 and A_new2[1] < 9:
+    count += 1
+
+print(count)
