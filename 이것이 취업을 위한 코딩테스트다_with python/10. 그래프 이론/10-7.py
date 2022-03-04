9af7fae63ab2bf28257dc3b22e65cbdaaff2bb42
inni <97818356+inni-iii@users.noreply.github.com>
2022-03-04 17:11:07 +0900

    2. 팀 결성

---
+++ 이것이 취업을 위한 코딩테스트다_with python/10. 그래프 이론/10-7.py
@@ -0,0 +1,11 @@
+# my_code
+
+n, m = map(int, input().split())
+team = [list(map(int, input().split())) for _ in range(m)]
+
+for i in range(m):
+    if team[i][0] == 1:
+        if team[i][1] == team[i][2]:
+            print('YES')
+        else:
+            print('NO')
