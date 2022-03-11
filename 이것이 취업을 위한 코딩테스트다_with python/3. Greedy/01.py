19512a6df81ee4da7e4f1b9aa3fd45b0665d4c2d
inni <97818356+inni-iii@users.noreply.github.com>
2022-03-11 17:03:40 +0900

    Q 01. 모험가 길드

---
+++ 이것이 취업을 위한 코딩테스트다_with python/3. Greedy/01.py
@@ -0,0 +1,30 @@
+# my_code
+
+n = int(input())
+scary = list(map(int, input().split()))
+scary.sort(reverse = True)
+
+num = n
+cnt = 0
+i = 0
+while True:
+    if num >= scary[i]:
+        if i == len(scary) - 1:
+            if scary[i] == 1:
+                cnt += 1
+                break
+            else:
+                break
+        else:
+            cnt += 1
+            num -= scary[i]
+            if num == 0:
+                break
+            else:
+                i += scary[i]
+            print(i)
+            print(num)
+    else:
+        break
+        
+print(cnt)
