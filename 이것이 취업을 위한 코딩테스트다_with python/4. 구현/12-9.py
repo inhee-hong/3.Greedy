dc55520979f3d42c05c44d7e5399b89db63b1a2b
inni <97818356+inni-iii@users.noreply.github.com>
2022-03-18 17:43:25 +0900

    Q 09. 문자열 압촉

--- 이것이 취업을 위한 코딩테스트다_with python/4. 구현/12-9.py
+++ 이것이 취업을 위한 코딩테스트다_with python/4. 구현/12-9.py
@@ -17,8 +17,13 @@ def solution(s):
                     n += str(cnt)
                     n += _len[a]
             elif _len[a] != _len[a+1] and a == len(_len) - 2:
-                n += _len[a]
-                n += _len[a+1]
+                if cnt != 1:
+                    n += str(cnt)
+                    n += _len[a]
+                    n += _len[a+1]
+                else:
+                    n += _len[a]
+                    n += _len[a+1]
             elif _len[a] == _len[a+1]:
                 cnt += 1
             elif _len[a] != _len[a+1]:
@@ -27,7 +32,7 @@ def solution(s):
                     n += _len[a]
                     cnt = 1
                 else:
-                    n += _len[a]             
+                    n += _len[a]    
 
         ans.append(len(n))
     ans.append(len(s)) 
