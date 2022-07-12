155d758ae0d8e40e5c8ca022f3551af72e4afc5e
inni <97818356+inni-iii@users.noreply.github.com>
2022-07-12 23:16:15 +0900

    색종이 만들기

--- coding with python/baekjoon/2630.py
+++ coding with python/baekjoon/2630.py
@@ -15,17 +15,69 @@ def answer(x, y, n):
     for i in range(x, x+n):
         for j in range(y, y+n):
             if now != paper[i][j]:
+                #print(0, '-', i, j, n, '/', x, y)
                 answer(x, y, n//2)
+                #print(1, '=',i, j, n, '/', x, y)
                 answer(x, y + n//2, n//2)
+                #print(2, '=',i, j, n, '/', x, y)
                 answer(x + n//2, y, n//2)
+                #print(3, '=',i, j, n, '/', x, y)
                 answer(x + n//2, y + n//2, n//2)
+                #print(4,'=',i, j, n, '/', x, y)
                 return
             
     if now == 0:
         white += 1
+        #print('white =', 1)
     else:
         blue += 1
+        #print('blue =', 1)
 
 answer(0, 0, n)
 print(white)
 print(blue)
+
+
+#  0 - 0 2 8 / 0 0
+#  0 - 0 2 4 / 0 0
+#  blue = 1
+#  1 = 0 2 4 / 0 0
+#  white = 1
+#  2 = 0 2 4 / 0 0
+#  white = 1
+#  3 = 0 2 4 / 0 0
+#  white = 1
+#  4 = 0 2 4 / 0 0
+#  1 = 0 2 8 / 0 0
+#  0 - 0 6 4 / 0 4
+#  white = 1
+#  1 = 0 6 4 / 0 4
+#  blue = 1
+#  2 = 0 6 4 / 0 4
+#  blue = 1
+#  3 = 0 6 4 / 0 4
+#  white = 1
+#  4 = 0 6 4 / 0 4
+#  2 = 0 2 8 / 0 0
+#  0 - 4 1 4 / 4 0
+#  0 - 4 1 2 / 4 0
+#  blue = 1
+#  1 = 4 1 2 / 4 0
+#  white = 1
+#  2 = 4 1 2 / 4 0
+#  white = 1
+#  3 = 4 1 2 / 4 0
+#  blue = 1
+#  4 = 4 1 2 / 4 0
+#  1 = 4 1 4 / 4 0
+#  white = 1
+#  2 = 4 1 4 / 4 0
+#  white = 1
+#  3 = 4 1 4 / 4 0
+#  blue = 1
+#  4 = 4 1 4 / 4 0
+#  3 = 0 2 8 / 0 0
+#  blue = 1
+#  4 = 0 2 8 / 0 0
+#  9
+#  7
